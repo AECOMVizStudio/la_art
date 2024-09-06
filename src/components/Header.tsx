700a22e0f25fb0978417d587061c0d3654db6915
Col-R <2colewestin@gmail.com>
2024-09-06 11:09:55 -0500

    header logo change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         
             <header className="p-4 flex flex-col items-center md:flex-row md:justify-center md:items-center">
-                <img src="/lashp_logo.png" alt="Los Angeles State Historic Park" id="lashplogo" />
+                <img src="/lashp_logo.webp" alt="Los Angeles State Historic Park" id="lashplogo" />
                 <h1 className="text-2xl font-bold text-center md:text-left">Los Angeles State Historic Park General Plan Amendment</h1>
             </header>
 
