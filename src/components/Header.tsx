ffdc6ec39ddf8db9708e31883781e50d7f322a10
Col-R <2colewestin@gmail.com>
2024-09-06 11:49:38 -0500

    tabindex for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 const Header = () => {
     return (
         
-            <header className="p-4 flex flex-col items-center md:flex-row md:justify-center md:items-center">
-                <img src="/lashp_logo.webp" alt="Los Angeles State Historic Park" id="lashplogo" />
+            <header tabIndex={0}  className="p-4 flex flex-col items-center md:flex-row md:justify-center md:items-center">
+                <img src="/lashp_logo.webp" alt="Los Angeles State Historic Park logo" id="lashplogo" />
                 <h1 className="text-2xl font-bold text-center md:text-left">Los Angeles State Historic Park General Plan Amendment</h1>
             </header>
 
