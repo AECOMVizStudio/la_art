4d6b5b371602b35e885270014ce129dde8b17a01
Col-R <2colewestin@gmail.com>
2024-09-06 09:28:20 -0500

    hero image work

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         <header className= "p-4 flex justify-around items-center">
           
-            <img src="/path/to/logo.png" alt="Los Angeles State Historic Park" />
+            <img src="/lashp_logo.png" alt="Los Angeles State Historic Park" id='lashplogo'/>
             <h1 className="text-2xl font-bold">Los Angeles State Historic Park General Plan Amendment</h1>
           
         </header>
