54286b1063e06e04e28b8650e5d653950fbc5115
Col-R <2colewestin@gmail.com>
2024-09-06 11:51:28 -0500

    unused code

--- src/components/tools/TopBanner.tsx
+++ src/components/tools/TopBanner.tsx
@@ -1,9 +1,9 @@
-import Translator from "./Translator";
+
 const TopBanner: React.FC = () => {
    return (
         <div className='bg-burnt-orange flex justify-center items-center'>
             <img src="/CATopBanner.png" alt="CAgov Logo" id='translation-logo' />
-            <Translator />
+            
         </div>
     );
     
