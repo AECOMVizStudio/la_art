57ef12148b3cdbda02d06be4a468f9edcd141c2b
Col-R <2colewestin@gmail.com>
2024-09-09 15:07:50 -0500

    Had to redo the entire component system because of irregularities in data type. Placement of links made it too complicated to maintain DRY

---
+++ src/components/children/Headings/GeneralPlan.tsx
@@ -0,0 +1,31 @@
+const GeneralPlan = () => {
+  return (
+    
+        <section 
+            tabIndex={0} 
+            
+            className="bg-slate-100 border-2 border-slate-200 rounded mb-8 p-8"
+        >
+
+        <h2 className="text-3xl font-bold underline text-burnt-orange">
+        Los Angeles State Historic Park (LASHP) General Plan Amendment (GPA) Process        
+        </h2>
+
+        <p className="font-medium pt-4">
+        California State Parks is initiating a GPA process for LASHP to review and consider direct transit access via the proposed Los Angeles Aerial Rapid Transit (LA ART) aerial gondola project.  If you wish to be involved and provide input, please sign up <a 
+            href='https://docs.google.com/forms/d/1dku_08vhyPRPaIfBE1B6JNZXp-qnZNz2UwVhYjKqgbk/viewform?edit_requested=true'
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 underline"
+            >
+            here.
+            </a>
+                
+                </p>
+    
+        </section>
+        
+  )
+}
+
+export default GeneralPlan
--- src/components/children/Subtitle.tsx
+++ src/components/children/Subtitle.tsx
@@ -1,62 +1,10 @@
-interface SectionItem {
-    id:number;
-    title:string;
-    subtitle?:string | string[];
-    question?: string[];
-    answer?: string[];
-    href:string;
-}
-
-const mainContent: SectionItem[] = [
-    {
-        id: 1,
-        title:'Upcoming Meetings',
-        subtitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate cumque officia laboriosam repudiandae perferendis quas eligendi velit nostrum accusamus necessitatibus!',
-
-        href: '/'
-    },
-    {   
-        id: 2,
-        title:'About the Project',
-        subtitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate cumque officia laboriosam repudiandae perferendis quas eligendi velit nostrum accusamus necessitatibus!',
-        href: '/'
-    },
-    {
-        id:3,
-        title:'General Plan Amendment',
-        subtitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate cumque officia laboriosam repudiandae perferendis quas eligendi velit nostrum accusamus necessitatibus!',
-        href: '/'
-    },
-    {
-        id:4,
-        title:'Current Project Status',
-        subtitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate cumque officia laboriosam repudiandae perferendis quas eligendi velit nostrum accusamus necessitatibus!',
-        href: '/'
-    },
-    {
-        id:5,
-        title:'FAQ',
-        subtitle: '',
-        question: ['Question 1', 'Question 2', 'Question 3'],
-        answer: ['Answer 1', 'Answer 2', 'Answer 3'],
-        href: '/'
-    },
-]
+import GeneralPlan from "./Headings/GeneralPlan"
 
 
 const Subtitle = () => {
     return (
         <div className="flex-col mx-2">
-
-            {mainContent.map((content) => (
-                <section tabIndex={0} key={content.id} className="bg-slate-100 border-2 border-slate-200 rounded mb-8 p-8">
-                    <h2 className="text-3xl font-bold underline text-burnt-orange">{content.title}</h2>
-                    <p className="font-medium">{content.subtitle}</p>
-                    {/* <a href={content.href}>Link here</a> */}
-                </section>
-            ))}
-
-
+            <GeneralPlan />
         </div>
   )
 }
