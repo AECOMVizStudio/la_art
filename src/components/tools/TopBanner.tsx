a40b95c280416a0a1d466ff047c11ece82a6d2f5
Col-R <2colewestin@gmail.com>
2024-09-10 13:15:59 -0500

    removed orange banner

--- src/components/tools/TopBanner.tsx
+++ src/components/tools/TopBanner.tsx
@@ -2,7 +2,6 @@ const TopBanner: React.FC = () => {
    return (
         <div className='bg-burnt-orange flex justify-center items-center'>
             <img src="/CATopBanner.png" alt="CAgov Logo" id='translation-logo' />
-            
         </div>
     );
     
