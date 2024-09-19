18c32f1768919638c0c8d22f8db18675070511f9
Col-R <2colewestin@gmail.com>
2024-09-19 12:16:48 -0500

    alignment and spacing changes. Converted to grid layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,28 +4,33 @@ function Footer() {
 
   
   return (
-    <footer className="bg-night text-light-grey p-4 md:px-0">
-      <div className="flex flex-col md:flex-row justify-evenly items-center 
-      md:mx-[12%]">
-        <div className="flex-col md:flex md:flex-row w-full md:w-1/4 items-center md:justify-start">
-          <img src="/CalStateParkLogo.png" alt="california government logo" id='footer-logo' loading="lazy"/>
-          <p className="md:px-4">© {formattedDate} Los Angeles State Historic Park</p>
-        </div>
-        
-        <ul className="w-full md:w-3/4 lg:flex lg:flex-row lg:justify-evenly py-2 md:pl-8">
-          <li>
-            <a href="https://www.parks.ca.gov/ConditionsOfUse" target="_blank" rel="noopener noreferrer" className="hover:underline">
-              Conditions of Use
-            </a>
-          </li>
-          <li>
-            <a href="https://www.parks.ca.gov/Privacy" target="_blank" rel="noopener noreferrer" className="hover:underline">
-              Privacy Policy
-            </a>
-          </li>
-        </ul>
+  <footer className="bg-night text-light-grey p-4 md:px-0">
+    <div className="grid md:grid-cols-3 gap-4 mx-auto md:mx-[12%] items-center">
+      
+      <div className="flex flex-col md:flex-row items-center justify-center md:justify-start py-4">
+        <img src="/CalStateParkLogo.png" alt="california government logo" id='footer-logo' loading="lazy"/>
+        <p className="md:px-4 text-center md:text-left">© {formattedDate} Los Angeles State Historic Park</p>
       </div>
-    </footer>
+      
+      <ul className="text-center md:text-left flex flex-col md:flex-row md:justify-between py-2">
+        <li className="mb-2 md:mb-0">
+          <a href="https://www.parks.ca.gov/ConditionsOfUse" target="_blank" rel="noopener noreferrer" className="hover:underline">
+            Conditions of Use
+          </a>
+        </li>
+        <li className="mb-2 md:mb-0">
+          <a href="https://www.parks.ca.gov/Privacy" target="_blank" rel="noopener noreferrer" className="hover:underline">
+            Privacy Policy
+          </a>
+        </li>
+      </ul>
+
+      
+      <div className="hidden md:block">
+      </div>
+
+    </div>
+  </footer>
   );
 }
 
--- src/components/children/Connections.tsx
+++ src/components/children/Connections.tsx
@@ -10,48 +10,58 @@ interface ConnectionsProps {
   
 function Connections({ followLinks }: ConnectionsProps) {
   return (
-    <div className="bg-dark-grey text-light-grey px-4 py-10 flex flex-col md:flex-row md:px-0 justify-start">
-      <div className="flex flex-col w-full md:w-1/4
-      md:mx-[12%] ">
-
-      <h3 className="text-lg font-normal mt-4">Follow Us</h3>
-          <ul className="flex space-x-4">
+    <div className="bg-dark-grey text-light-grey px-4 py-10 md:px-0">
+      <div className="grid md:grid-cols-3 gap-4 mx-auto md:mx-[12%] items-start">
+        
+        
+        <div className="flex flex-col items-center md:items-start">
+          <h3 className="text-lg font-normal mt-4">Follow Us</h3>
+          <ul className="flex space-x-4 mt-4">
             <li>
               <a href={followLinks.facebook} target="_blank" rel="noopener noreferrer">
-                <img src="./icons/facebook.svg" className="svg" alt="facebook"></img>
+                <img src="./icons/facebook.svg" className="svg" alt="facebook" />
               </a>
             </li>
             <li>
               <a href={followLinks.twitter} target="_blank" rel="noopener noreferrer">
-                <img src="./icons/x.svg" className="svg" alt="x.com"></img>
+                <img src="./icons/x.svg" className="svg" alt="x.com" />
               </a>
             </li>
             <li>
               <a href={followLinks.youtube} target="_blank" rel="noopener noreferrer">
-                <img src="./icons/youtube.svg" className="svg" alt="youtube"></img>
+                <img src="./icons/youtube.svg" className="svg" alt="youtube" />
               </a>
             </li>
             <li>
               <a href={followLinks.instagram} target="_blank" rel="noopener noreferrer">
-                <img src="./icons/instagram.svg" className="svg" alt="instagram"></img>
+                <img src="./icons/instagram.svg" className="svg" alt="instagram" />
               </a>
             </li>
             <li>
-              <a className="bg-white" href={followLinks.wordpress} target="_blank" rel="noopener noreferrer">
-                <img src="./icons/wordpress.svg" className="svg" alt="wordpress"></img>
+              <a href={followLinks.wordpress} target="_blank" rel="noopener noreferrer">
+                <img src="./icons/wordpress.svg" className="svg" alt="wordpress" />
               </a>
             </li>
           </ul>
-      </div>
-
-      <div className="flex flex-col w-full md:w-1/4">
-
-          <h3 className="text-lg font-normal mt-4">Contact Us</h3>
-          <p className="">Have a question? Contact us <a className="underline" href="https://www.parks.ca.gov/?page_id=23110" target="_blank" rel="noopener noreferrer">here</a></p>
-      </div>
-
+        </div>
+        
       
+        <div className="flex flex-col items-center md:items-start md:col-span-2">
+          <h3 className="text-lg font-normal mt-4">Contact Us</h3>
+          <p className="mt-4 text-center md:text-left">
+            Have a question? Contact us
+            <a className="underline ml-1" href="https://www.parks.ca.gov/?page_id=23110" target="_blank" rel="noopener noreferrer">here</a>
+          </p>
+        </div>
+        
+
+        <div className="hidden md:block">
+
+        </div>
+
+      </div>
     </div>
+
   );
 }
   
