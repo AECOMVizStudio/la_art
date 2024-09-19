ce6ad2abe3faff515a69042520672fa627ea6df4
Col-R <2colewestin@gmail.com>
2024-09-19 14:28:12 -0500

    right arrows added to ul items

--- src/components/children/Sidebar.tsx
+++ src/components/children/Sidebar.tsx
@@ -8,18 +8,36 @@ const relatedPages:Pages[] = [
   {name: "Archaeological Excavation at LASHP ", link: "https://www.parks.ca.gov/?page_id=25540"},
   {name: "LASHP General Plan", link: "https://lastatehistoricpark.org/wp-content/uploads/2021/07/LASHP-General-Plan.pdf"},
   {name: "Volunteer with LA River State Park Partners", link: "https://www.parks.ca.gov/?page_id=25858"},
-  
 ];
+
+interface Mail {
+  abbreviation: string;
+  text: string;
+  href?: string;
+}
+
+const mailingList:Mail[] = [
+  {abbreviation: "EN", text: "Join our mailing list!", href: "https://docs.google.com/forms/d/1dku_08vhyPRPaIfBE1B6JNZXp-qnZNz2UwVhYjKqgbk/viewform?edit_requested=true"},
+  {abbreviation: "ZH", text: "加入我们的邮件列表!"},
+  {abbreviation: "ZH", text: "加入我們的郵件清單!"},
+  {abbreviation: "SP", text: "¡Únase a nuestra lista de correo!"},
+]
+
+const rightChevron = 
+  (<svg className="carat" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="12px" height="12px">
+  <path d="M9 6l6 6-6 6z"/>
+  </svg>)
 
 const Sidebar = () => {
   
     return (
-      <div className="flex-row lg:flex-col">
-          <div className="p-6">
-            <h3 className="text-lg font-bold">Related Pages</h3>
-            <ul className="list-disc">
+      <div className="flex-row lg:flex-col p-6">
+          <div className="py-6">
+            <h3 className="text-xl font-bold">Related Pages</h3>
+            <ul className="list-none">
               {relatedPages.map((page) => (
-                <li key={page.name} className="p-2 mx-6 underline">
+                <li key={page.name} className="p-2 border-b flex flex-row items-center">
+                  {rightChevron}
                   <a href={page.link} target="_blank" rel="noopener noreferrer">
                     {page.name}
                   </a>
@@ -27,11 +45,17 @@ const Sidebar = () => {
               ))}
             </ul>
           </div>
-            <div className="p-6">
-              <h3 className="text-lg font-bold">Join our email list!</h3>
-              <a href="https://docs.google.com/forms/d/1dku_08vhyPRPaIfBE1B6JNZXp-qnZNz2UwVhYjKqgbk/viewform?edit_requested=true" target="_blank" rel="noopener noreferrer" className="text-medium-blue hover:underline">
-              Sign up to receive updates on the LASHP General Plan Amendment
-              </a>
+            <div className="py-6">
+              <h3 className="text-xl font-bold">Sign up to receive LASHP General Plan Amendment information</h3>
+              {mailingList.map((item) => (
+                <div className="bg-gray-200 flex flex-row my-4 p-4 text-medium-blue rounded-md border border-gray-300">
+                  <span className="bg-medium-blue text-white 
+                                    language_icon">{item.abbreviation}</span>
+                    <a href={item.href} target="_blank" rel="noopener noreferrer" className="mx-2">
+                  {item.text}
+                  </a>
+                </div>
+              ))}
             </div>
         </div>
       );
