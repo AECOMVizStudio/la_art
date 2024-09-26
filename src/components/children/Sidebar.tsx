9686e60d6ed1529e66b0944fd8f9156aa2429e2e
Col-R <2colewestin@gmail.com>
2024-09-26 10:26:08 -0500

    changed sidebar text

--- src/components/children/Sidebar.tsx
+++ src/components/children/Sidebar.tsx
@@ -18,10 +18,10 @@ interface Mail {
 }
 
 const mailingList:Mail[] = [
-  {language: "english", abbreviation: "EN", text: "Join our mailing list!", href: "https://docs.google.com/forms/d/1dku_08vhyPRPaIfBE1B6JNZXp-qnZNz2UwVhYjKqgbk/viewform?edit_requested=true"},
-  {language: "simplified chinese", abbreviation: "ZH", text: "加入我们的邮件列表!", href:"https://docs.google.com/forms/d/e/1FAIpQLSeKPyphpezD3_VX4I49bvAAgXd1FKVexxy3DBsbVNM5A0bwUA/viewform"},
-  {language: "traditional chinese", abbreviation: "ZH", text: "加入我們的郵件清單!", href:"https://docs.google.com/forms/d/e/1FAIpQLSeX92ce_SL2oJKzNWLA15Bjw7sV_-ATaq90xX_xawXxfD_u2g/viewform"},
-  {language: "spanish", abbreviation: "SP", text: "¡Únase a nuestra lista de correo!", href:"https://docs.google.com/forms/d/e/1FAIpQLSdVhQQu7Rf8vcRL8-JFYcHCJSrsXgTwBgsSCDUwZV6OwKKtSA/viewform"},
+  {language: "english", abbreviation: "EN", text: "Join our email list!", href: "https://docs.google.com/forms/d/1dku_08vhyPRPaIfBE1B6JNZXp-qnZNz2UwVhYjKqgbk/viewform?edit_requested=true"},
+  {language: "simplified chinese", abbreviation: "ZH", text: "加入我们的邮件订阅列表!", href:"https://docs.google.com/forms/d/e/1FAIpQLSeKPyphpezD3_VX4I49bvAAgXd1FKVexxy3DBsbVNM5A0bwUA/viewform"},
+  {language: "traditional chinese", abbreviation: "ZH", text: "加入我們的電子郵件訂閱名單！", href:"https://docs.google.com/forms/d/e/1FAIpQLSeX92ce_SL2oJKzNWLA15Bjw7sV_-ATaq90xX_xawXxfD_u2g/viewform"},
+  {language: "spanish", abbreviation: "SP", text: "¡Únase a nuestra lista de email!", href:"https://docs.google.com/forms/d/e/1FAIpQLSdVhQQu7Rf8vcRL8-JFYcHCJSrsXgTwBgsSCDUwZV6OwKKtSA/viewform"},
 ]
 
 const rightChevron = 
