57f0ed3fe50305ddfdd5d1ad482e80015430add0
Col-R <2colewestin@gmail.com>
2024-09-27 08:51:27 -0500

    temporary change to upcoming

--- src/components/children/Headings/Upcoming.tsx
+++ src/components/children/Headings/Upcoming.tsx
@@ -14,7 +14,7 @@ const Upcoming = () => {
           </h2>
 
           <p className="font-medium pt-4">The LASHP GPA public outreach effort is scheduled to begin in October 2024 and will extend into Spring 2025.</p>
-          <p className="font-medium pt-4">California State Parks will be hosting two initial public meetings to provide information on the LASHP GPA process and the LA ART project, and to collect input on ideas and suggestions to be considered during the GPA process. These two meetings will cover the same content. </p>
+          <p className="font-medium pt-4">If you wish to be involved and provide input, please sign up by selecting your preferred language on the right-hand side of the page under "Sign up to receive LASHP General Plan Amendment information." </p>
       
           </section>
           
