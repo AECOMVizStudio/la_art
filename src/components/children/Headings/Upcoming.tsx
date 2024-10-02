1c5e75305b09b8915e7a874a201cfd24a8f073f6
Col-R <2colewestin@gmail.com>
2024-10-02 09:01:54 -0500

    text update

--- src/components/children/Headings/Upcoming.tsx
+++ src/components/children/Headings/Upcoming.tsx
@@ -13,7 +13,7 @@ const Upcoming = () => {
           Upcoming Meetings       
           </h2>
 
-          <p className="font-medium pt-4">The LASHP GPA public outreach effort is scheduled to begin in October 2024 and will extend into Spring 2025.</p>
+          <p className="font-medium pt-4">The LASHP GPA public outreach effort is scheduled to begin in Fall 2024 and will extend into Spring 2025.</p>
           <p className="font-medium pt-4">If you wish to be involved and provide input, please sign up by selecting your preferred language on the right-hand side of the page under "Sign up to receive LASHP General Plan Amendment information." </p>
       
           </section>
