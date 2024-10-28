f9d9cd32044191391b33fdfb618e3e37a137cfb0
Col-R <2colewestin@gmail.com>
2024-10-28 09:29:08 -0500

    meeting info

--- src/components/children/Headings/Upcoming.tsx
+++ src/components/children/Headings/Upcoming.tsx
@@ -11,13 +11,16 @@ const Upcoming = () => {
           <h2 className="text-3xl yeseva font-bold text-burnt-orange">
           Upcoming Meetings       
           </h2>
+          <div className="bg-gradient-to-br from-[rgba(214,128,0,0.75)] to-[rgba(227,175,55,0.75)] border-2 border-gray-200 rounded-lg p-2 md:px-4">
 
-          <p className="font-medium pt-4">The LASHP GPA public outreach effort is scheduled to begin in Fall 2024 and will extend into Spring 2025.</p>
-          <p className="font-medium py-4">California State Parks will be hosting two initial public meetings to provide information on the LASHP GPA process and the LA ART project, and to collect input on ideas and suggestions to be considered during the GPA process. These two meetings will cover the same content.</p>
+            <p className="font-medium pt-4">The LASHP GPA public outreach effort will begin in November 2024 and will extend into Spring 2025.</p>
+            <div className="font-medium pt-4">If you wish to receive plan and meeting notifications, please sign up by clicking the button under “Sign up to receive LASHP General Plan Amendment information.” </div>
+            <p className="font-medium pt-4">California State Parks will be hosting two initial public meetings to provide information on the LASHP GPA process and the LA ART project, and to collect input on ideas and suggestions to be considered during the GPA process. These two meetings will cover the same content. In addition, an online form will be available to provide input following the meetings.</p>
+            <p className="font-medium pt-4">Interpretation will be provided at the Public Meetings in Spanish, Cantonese, Mandarin, and Taishanese.  For ADA accommodations, please call (XXX) XXX-XXXX at least 72 hours before either meeting.</p>
+            <p className="font-medium pt-4">Materials for the General Plan Amendment Public Meetings will be available on Thursday, November 7 on this website. Translated materials will be available in Spanish, Chinese (Traditional), and Chinese (Simplified).</p>
 
-            <div className="bg-gradient-to-br from-[rgba(214,128,0,0.75)] to-[rgba(227,175,55,0.75)] border-2 border-gray-200 rounded-lg p-2 md:px-4">
+            
                 
-
                 <div>
                     <h4 className="font-semibold pt-2 text-lg">Virtual Meeting: Los Angeles State Historic Park General Plan Amendment</h4>
                     <p className="font-normal pt-4">Thursday, November 7, 2024, 6:00 - 7:00 PM </p>
@@ -39,10 +42,6 @@ const Upcoming = () => {
                             Fred Harvey at Los Angeles Union Station, 800 Alameda Street, CA, 90012  
                         </div>
                         <div className="py-1">Parking validation will be available.</div>
-                        <div className="py-1">For ADA accommodations, please call (XXX) XXX-XXXX at least 72 hours before the meeting. </div>
-                        <div className="py-1">Materials for the GPA Public Meetings will be available on Thursday, October 17 on this website. Translated materials will be available in Spanish, Chinese (Simplified), and Chinese (Traditional). </div>
-                        <div className="py-1">Interpretation will be provided at the Public Meetings in Spanish, Cantonese, Mandarin, and Taishanese.</div>
-                        <div className="py-1">If you wish to receive plan and meeting notifications, please sign up by clicking the button under “Sign up to receive LASHP General Plan Amendment information."</div>
                 </div>
 
                 {/* <img src="/CalStateParkLogo.png" alt="" className="absolute opacity-15" id="upcomingWatermark"></img> */}
