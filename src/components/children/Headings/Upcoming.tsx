a2eae2e10c6574e009759153f17feb758bb944ba
Col-R <2colewestin@gmail.com>
2024-10-28 11:27:03 -0500

    text update

--- src/components/children/Headings/Upcoming.tsx
+++ src/components/children/Headings/Upcoming.tsx
@@ -16,7 +16,7 @@ const Upcoming = () => {
             <p className="font-medium pt-4">The LASHP GPA public outreach effort will begin in November 2024 and will extend into Spring 2025.</p>
             <div className="font-medium pt-4">If you wish to receive plan and meeting notifications, please sign up by clicking the button under “Sign up to receive LASHP General Plan Amendment information.” </div>
             <p className="font-medium pt-4">California State Parks will be hosting two initial public meetings to provide information on the LASHP GPA process and the LA ART project, and to collect input on ideas and suggestions to be considered during the GPA process. These two meetings will cover the same content. In addition, an online form will be available to provide input following the meetings.</p>
-            <p className="font-medium pt-4">Interpretation will be provided at the Public Meetings in Spanish, Cantonese, Mandarin, and Taishanese.  For ADA accommodations, please call (XXX) XXX-XXXX at least 72 hours before either meeting.</p>
+            <p className="font-medium pt-4">Interpretation will be provided at the Public Meetings in Spanish, Cantonese, Mandarin, and Taishanese.  For ADA accommodations, please email planning@parks.ca.gov at least 72 hours before either meeting.</p>
             <p className="font-medium py-4">Materials for the General Plan Amendment Public Meetings will be available on Thursday, November 7 on this website. Translated materials will be available in Spanish, Chinese (Traditional), and Chinese (Simplified).</p>
 
             
