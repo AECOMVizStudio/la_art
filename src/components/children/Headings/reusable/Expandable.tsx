25ebbd22df38b6b7bcfc18d38023eb1124466c6b
Col-R <2colewestin@gmail.com>
2024-11-12 12:03:02 -0600

    added href to links on general plan expandable mobile version

--- src/components/children/Headings/reusable/Expandable.tsx
+++ src/components/children/Headings/reusable/Expandable.tsx
@@ -17,8 +17,8 @@ const Expandable = ({ props }: ExpandableProps) => {
         To view a diagram of the LASHP GPA Process, click on your preferred language, below:
       </p>
       {/* mobile view */}
-      <div className='sm:hidden' onClick={expandHandler}>
-        <div className="bg-gray-200 flex justify-between p-2">
+      <div className='sm:hidden' >
+        <div className="bg-gray-200 flex justify-between p-2" onClick={expandHandler}>
           <p className='text-center font-bold'>Click to expand</p>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 transition-transform duration-300 ease-in-out" fill="none" viewBox="0 0 24 24" stroke="currentColor"
             style={{ transform: expanded ? 'rotate(180deg)' : 'rotate(0deg)' }}
@@ -31,7 +31,8 @@ const Expandable = ({ props }: ExpandableProps) => {
           {props.map((diagram) => {
             return (
               <div key={diagram.language} className="flex justify-start items-center p-2 text-blue-500 underline border-2">
-                {diagram.text}
+                <a href={diagram.href} target="_blank" rel="noopener noreferrer">{diagram.text}</a>
+                
               </div>
             )
           })}
