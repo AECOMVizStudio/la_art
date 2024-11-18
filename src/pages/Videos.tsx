154fac84db537bf8ff75cbf218aed1239413fe1b
Col-R <2colewestin@gmail.com>
2024-11-18 15:49:49 -0600

    video thumbnails added, header and footer component imports for Videos main component added

---
+++ src/pages/Videos.tsx
@@ -0,0 +1,17 @@
+import Translator from "../components/tools/Translator";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+
+const Videos = () => {
+  return (
+    <>
+    <Translator />
+    <Header />
+    
+    <Footer />
+    </>
+  )
+}
+
+export default Videos
