3e31fc37f01457bd419abb3f466d6b474917dd7e
Col-R <2colewestin@gmail.com>
2024-11-19 14:11:08 -0600

    text updates

--- src/components/children/Headings/Background.tsx
+++ src/components/children/Headings/Background.tsx
@@ -14,7 +14,7 @@ const Background = () => {
           Zero Emissions Transit (ZET) and LA ARTT LLC are proposing the LA ART project to connect the regional transit system from Los Angeles Union Station to Dodger Stadium. The project would also include an intermediate station at the southernmost entrance of LASHP. The project would provide access between and connect El Pueblo de Los Angeles, Union Station, Chinatown, the LASHP, Mission Junction, Dodger Stadium, Solano Canyon, and Elysian Park to the regional transit system accessible at Union Station.</p>
 
           <p className="font-medium pt-4">
-          In February 2024, the Los Angeles County Metropolitan Transportation Authority (Metro) approved the proposed LA ART project and certified its Final Environmental Impact Report with mitigation measures and conditions, including this next step in the process: the LASHP GPA. </p>
+          In February 2024, the Los Angeles County Metropolitan Transportation Authority (Metro) approved the proposed LA ART project and certified its Final Environmental Impact Report with mitigation measures and conditions, including this next step in the process: the LASHP GPA.</p>
 
           <p className="font-medium pt-4">
           For more information on the LA ART project see the project website at <a 
