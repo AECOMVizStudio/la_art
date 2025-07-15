108149680516425fa44429d02844e52019743871
Col-R <2colewestin@gmail.com>
2025-07-15 12:59:01 -0500

    main section and about section updated, moved expandable to About section

--- src/components/children/Headings/About.tsx
+++ src/components/children/Headings/About.tsx
@@ -1,8 +1,38 @@
+import Expandable from './reusable/Expandable';
+import { DiagramLanguage } from '../../../interfaces';
+
 const About = () => {
+
+  const diagrams: DiagramLanguage[] = [
+  {
+    language: 'English',
+    abbreviation: 'EN',
+    text: 'English',
+    href: '/pdf/General Plan Amendment Process.pdf',
+  },
+  {
+    language: 'Spanish',
+    abbreviation: 'SP',
+    text: 'Spanish',
+    href: '/pdf/GPA Process_sp_accessible.pdf',
+  },
+  {
+    language: 'Traditional Chinese',
+    abbreviation: 'ZH',
+    text: 'Chinese - Traditional',
+    href: '/pdf/GPA Process_tc_accessible.pdf',
+  },
+  {
+    language: 'Simplified Chinese',
+    abbreviation: 'ZH',
+    text: 'Chinese - Simplified ',
+    href: '/pdf/GPA Process_sc_accessible.pdf',
+  },
+];
   return (
     <section tabIndex={0} className="border-t border-gray-300 mb-8 p-8">
       <h2 className="text-3xl yeseva font-bold text-burnt-orange">
-        About the GPA Process
+        About the GPA Process and Outreach
       </h2>
 
       <p className="font-medium pt-4">
@@ -12,22 +42,38 @@ const About = () => {
       </p>
 
       <p className="font-medium pt-4">
-        State Parks will gather public input and work with LA Aerial Rapid
+        State Parks will gather public input and work with the project sponsor for the LA ART project, LA Aerial Rapid
         Transit Technologies, LLC (LA ARTT LLC) to identify and address the
         potential issues and benefits of the project in order to consider a
-        draft GPA proposal in 2025. Once State Parks determines the GPA meets
+        draft GPA. Once State Parks determines the GPA meets
         the required standards and conditions for consideration of action, the
         GPA would be scheduled for a State Park and Recreation Commission (SPRC)
-        meeting agenda.
+        hearing.
       </p>
       <p className="font-medium pt-4">
-        State Parks' GPA process will involve a public outreach effort to gather
+        State Parks' GPA process involves a public outreach effort to gather
         input to help inform the Department's consideration of the LASHP GPA and
         the LA ART project. There will be multiple opportunities for the public
         to comment and provide input to the proposed action. California State
         Parks will conduct several public meetings throughout the GPA process to
-        provide information and receive comment and input.
+        provide information and receive comments and input.
       </p>
+      <Expandable props={diagrams} />
+
+      <p className="font-medium pt-4">
+        Questions can be directed to State Parks via email at lastatehistoricparkgpa@parks.ca.gov or by mail at: 
+      </p>
+      <div className="font-normal pt-4 mx-4">
+        <div className="py-2">
+          <p className="font-bold">
+            Strategic Planning and Recreation Services Division
+          </p>
+          <p>c/o LASHP GPA</p>
+          <p>PO Box 942896 </p>
+          <p>Sacramento CA 94296-0001</p>
+        </div>
+      </div>
+      
     </section>
   );
 };
--- src/components/children/Headings/GeneralPlan.tsx
+++ src/components/children/Headings/GeneralPlan.tsx
@@ -1,87 +1,16 @@
-import Expandable from './reusable/Expandable';
-import { DiagramLanguage } from '../../../interfaces';
-
-// this should be done via looping through an array of objects with the section's data, but I couldn't get it to work in time, so they are just hardcoded.
-// The placement of the links within the body text was proving to be the issue. Maybe if it was concatenated? I'll have to revisit this.
-
-const diagrams: DiagramLanguage[] = [
-  {
-    language: 'English',
-    abbreviation: 'EN',
-    text: 'English',
-    href: '/pdf/General Plan Amendment Process.pdf',
-  },
-  {
-    language: 'Spanish',
-    abbreviation: 'SP',
-    text: 'Spanish',
-    href: '/pdf/GPA Process_sp_accessible.pdf',
-  },
-  {
-    language: 'Traditional Chinese',
-    abbreviation: 'ZH',
-    text: 'Chinese - Traditional',
-    href: '/pdf/GPA Process_tc_accessible.pdf',
-  },
-  {
-    language: 'Simplified Chinese',
-    abbreviation: 'ZH',
-    text: 'Chinese - Simplified ',
-    href: '/pdf/GPA Process_sc_accessible.pdf',
-  },
-];
 
 const GeneralPlan = () => {
   return (
     <section tabIndex={0} className="mb-4 p-8">
       <h2 className="text-3xl yeseva font-bold text-burnt-orange">
         Los Angeles State Historic Park (LASHP) General Plan Amendment (GPA)
-        Process
       </h2>
       <p className="font-medium pt-4">
-        California State Parks is initiating a GPA process for LASHP to review
-        and consider direct transit access via the proposed Los Angeles Aerial
-        Rapid Transit (LA ART) aerial gondola project. If you wish to be
+       California State Parks is considering an amendment to the Los Angeles State Historic Park (LASHP) General Plan (GPA). This proposed GPA would add a transit land use to the General Plan, allowing for consideration and review of the proposed Los Angeles aerial rapid transit gondola project. If you wish to be
         involved and provide input, please join our email list by clicking the
         button under “Sign up to receive LASHP General Plan Amendment
         information.”
       </p>
-      <p className="font-medium pt-4">
-        Input that was provided by December 15, 2024, will be considered as
-        State Parks develops draft GPA options. However, State Parks continues
-        to welcome additional feedback and input throughout the GPA process. An
-        online input form is available
-        <a
-          href="https://www.surveymonkey.com/r/lastatehistoricparkgpa"
-          target="_blank"
-          className="text-blue-500 underline font-bold"
-        >
-          {' '}
-          here
-        </a>
-        . Questions can also be directed to State Parks via email at
-        lastatehistoricparkgpa@parks.ca.gov or by mail at:
-      </p>
-      <div className="font-normal pt-4 mx-4">
-        <div className="py-2">
-          <p className="font-bold">
-            Strategic Planning and Recreation Services Division
-          </p>
-          <p>c/o LASHP GPA</p>
-          <p>PO Box 942896 Sacramento CA 94296-0001</p>
-        </div>
-      </div>
-      <p className="font-medium pt-4">
-        {/* <span className="font-bold">
-          This initial input period will be open until 11:59 pm Pacific Time on
-          Sunday, December 15.
-        </span> */}
-        There will be additional opportunities to ask questions and share input
-        at future public meetings, with the next sessions scheduled for early
-        2025. Updates about public meetings will be shared on this webpage and
-        via email and social media.
-      </p>
-      <Expandable props={diagrams} />
     </section>
   );
 };
